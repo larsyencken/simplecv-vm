9e965a76d95a499189a0a78ba9ae617657247e7b
Lars Yencken <lars@yencken.org>
2013-11-04 11:17:53 +1100

    Scripts to bootstrap are now on Github.

--- setup_simplecv.sh
+++ setup_simplecv.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -e
 
-# install marelle
-sudo apt-get update
-sudo apt-get install -y curl git swi-prolog-nox
-git clone https://bitbucket.org/larsyencken/marelle /usr/local/marelle
+if [ ! -x /usr/bin/curl ]; then
+  sudo apt-get update
+  sudo apt-get install -y curl
+fi
+curl -s "https://raw.github.com/larsyencken/marelle/master/bootstrap_global.sh" | bash
 
-cat >/usr/local/bin/marelle <<EOF
-#!/bin/bash
-exec swipl -q -t main -s /usr/local/marelle/marelle.pl "\$@"
-EOF
-chmod a+x /usr/local/bin/marelle
-
-# get some deps
-git clone https://bitbucket.org/larsyencken/marelle-deps
+if [ ! -d marelle-deps ]; then
+  # get some deps
+  git clone https://github.com/larsyencken/marelle-deps
+fi
 
 marelle meet \
   simplecv \
