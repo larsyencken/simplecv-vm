18dc901452a44650e8409624ece0c712603df389
Lars Yencken <lars@yencken.org>
2014-05-18 21:53:52 +1000

    Update the bootstrap URL.

--- setup_simplecv.sh
+++ setup_simplecv.sh
@@ -4,7 +4,7 @@ if [ ! -x /usr/bin/curl ]; then
   sudo apt-get update
   sudo apt-get install -y curl
 fi
-curl -s "https://raw.github.com/larsyencken/marelle/master/bootstrap_global.sh" | bash
+curl -s "https://raw.githubusercontent.com/larsyencken/marelle/master/bootstrap_global.sh" | bash
 
 if [ ! -d marelle-deps ]; then
   # get some deps
