4ddfe744f04f30668b1baa9158b1f1ad0d21736e
Lars Yencken <lars@yencken.org>
2014-07-29 17:24:46 +1000

    Bootstrap trusty using a shell script.

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,10 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-Vagrant::Config.run do |config|
-  config.vm.box = "precise32"
-  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu/trusty32"
   config.vm.provision :shell, :path => "setup_simplecv.sh"
 end
--- setup_simplecv.sh
+++ setup_simplecv.sh
@@ -1,18 +1,51 @@
 #!/bin/bash -e
+#
+#  setup_simplecv.sh
+#
+#  Install the SimpleCV package and the IPython notebook on Ubuntu 14.04.
+#
 
-if [ ! -x /usr/bin/curl ]; then
-  sudo apt-get update
-  sudo apt-get install -y curl
-fi
-curl -s "https://raw.githubusercontent.com/larsyencken/marelle/master/bootstrap_global.sh" | bash
+APT_UPDATED=0
 
-if [ ! -d marelle-deps ]; then
-  # get some deps
-  git clone https://github.com/larsyencken/marelle-deps
-fi
+function has_exec() {
+  which "$1" &>/dev/null
+}
 
-marelle meet \
-  simplecv \
-  python-recommended \
-  vim \
-  dotvim
+function has_python_package() {
+  python -c "import $1" &>/dev/null
+}
+
+function has_apt_package() {
+  dpkg -l | grep -q "^$1$"
+}
+
+function apt_install() {
+  if [ $APT_UPDATED -eq 0 ]; then
+    sudo apt-get update
+    APT_UPDATED=1
+  fi
+
+  sudo apt-get install -y "$1"
+}
+
+function apt_check_install() {
+  for pkg in "$@"; do
+    has_apt_package "$pkg" || apt_install "$pkg"
+  done
+}
+
+function install_simplecv() {
+  apt_check_install \
+    build-essential \
+    ipython-notebook \
+    python-dev \
+    python-numpy \
+    python-opencv \
+    python-pip \
+    python-pygame \
+    python-scipy \
+
+  sudo pip install SimpleCV
+}
+
+has_python_package SimpleCV || install_simplecv
